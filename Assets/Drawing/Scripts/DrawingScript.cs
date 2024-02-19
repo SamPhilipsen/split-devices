37f88496d360ddf8d20a271a91bbbf8399ce6c65
Philipsen,Sam S <s.philipsen@student.fontys.nl>
2024-02-19 15:35:28 +0100

    Add drawing on screen

---
+++ Assets/Drawing/Scripts/DrawingScript.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DrawingScript : MonoBehaviour
+{
+    public Camera m_camera;
+    public GameObject brush;
+    public DrawingManager drawingManager;
+
+    LineRenderer currentLineRenderer;
+
+    Vector2 lastPos;
+
+    private void Update()
+    {
+        Drawing();
+    }
+
+
+    void Drawing()
+    {
+        if (CanDraw.instance.IsHoveringOverUI()) {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            CreateBrush();
+        }
+        else if (Input.GetKey(KeyCode.Mouse0))
+        {
+            PointToMousePos();
+        }
+        else
+        {
+            currentLineRenderer = null;
+        }
+    }
+
+    void CreateBrush()
+    {
+        GameObject brushInstance = Instantiate(brush);
+        DrawingManager.instance.AddLine(brushInstance);
+        currentLineRenderer = brushInstance.GetComponent<LineRenderer>();
+
+        //because you gotta have 2 points to start a line renderer, 
+        Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+
+        currentLineRenderer.SetPosition(0, mousePos);
+        currentLineRenderer.SetPosition(1, mousePos);
+    }
+
+    void AddAPoint(Vector2 pointPos)
+    {
+        currentLineRenderer.positionCount++;
+        int positionIndex = currentLineRenderer.positionCount - 1;
+        currentLineRenderer.SetPosition(positionIndex, pointPos);
+    }
+
+    void PointToMousePos()
+    {
+        Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+        if (lastPos != mousePos)
+        {
+            AddAPoint(mousePos);
+            lastPos = mousePos;
+        }
+    }
+
+}
