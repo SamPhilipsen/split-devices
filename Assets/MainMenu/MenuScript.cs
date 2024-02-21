8437f19cd07684d48b2bba7fe1857a6a9f1fd344
NachoCheese <ash.hooijmans@gmail.com>
2024-02-21 13:23:58 +0100

    Added instructions to the menu

--- Assets/MainMenu/MenuScript.cs
+++ Assets/MainMenu/MenuScript.cs
@@ -1,21 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
-    public void PlayHidenSeek()
+    public GameObject mainMenu;
+    public GameObject Instructions;
+
+    public Button snapnguess;
+    public Button snapnpose;
+    public Button snapncopy;
+    public Button goback;
+    public Button play;
+
+    public void Start()
     {
-        SceneManager.LoadScene("SampleScene");
+        snapnguess.onClick.AddListener(ShowGuess);
+
+        snapnpose.onClick.AddListener(ShowPose);
+
+        snapncopy.onClick.AddListener(ShowCopy);
+
+        goback.onClick.AddListener(GoBack);
+        play.onClick.AddListener(Play);
+
     }
 
-    public void PlayOutliner()
+    public void Play()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("CamTest 1");
     }
 
-    public void PlayMemory()
+    public void ShowGuess()
     {
-        SceneManager.LoadScene("SampleScene");
+        Instructions.SetActive(true);
+        HideMenu();
+
+        GameObject instructionsWindow = GameObject.Find("Snap n Guess");
+        instructionsWindow.SetActive(true);
+    }
+
+    public void ShowPose()
+    {
+        Instructions.SetActive(true);
+        HideMenu();
+
+        GameObject instructionsWindow = GameObject.Find("Snap n Pose");
+        instructionsWindow.SetActive(true);
+    }
+
+    public void ShowCopy()
+    {
+        Instructions.SetActive(true);
+        HideMenu();
+
+        GameObject instructionsWindow = GameObject.Find("Snap n Copy");
+        instructionsWindow.SetActive(true);
+    }
+
+    public void GoBack()
+    {
+        HideInstructions();
+        ShowMenu();
+    }
+
+    public void HideInstructions()
+    {
+        Instructions.SetActive(false);
+        HideMenu();
+    }
+
+
+    public void HideMenu()
+    {
+        mainMenu.SetActive(false);
+    }
+
+    public void ShowMenu()
+    {
+        mainMenu.SetActive(true);
     }
 }
